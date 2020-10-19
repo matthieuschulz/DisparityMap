c6f3ca20a023779d95e2cc13012b22af5199f4a0
Matthieu Schulz <m.schulz18@ejm.org>
2020-10-19 03:35:40 +0100

    updated disparity.py with potential fun

--- src/disparity.py
+++ src/disparity.py
@@ -1,1 +1,12 @@
-print("test")
+def read_images():
+    pass
+def generate_map(): #implementation of alogorithm from the research paper
+    pass
+
+def square(a):
+    return a * a
+
+def min():
+    pass
+
+
